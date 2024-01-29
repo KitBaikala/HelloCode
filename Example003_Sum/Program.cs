24de03741070efbe4df254ac6afd29a681ccf56f
Andrei <andrei.yakimyuk>
2024-01-29 21:36:59 +0800

    Examples 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB; // Данная строка - контейнер для сохранения промежуточного результата. данную строку в данной задаче, можно было и не делать.
+Console.WriteLine(result);
+
